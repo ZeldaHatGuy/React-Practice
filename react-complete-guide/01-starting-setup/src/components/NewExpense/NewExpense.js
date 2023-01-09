1fb14ed6cfb84b0a41379122de29ae58a0317de8
ZeldaHatGuy <Equin0xx@yahoo.com>
2023-01-09 17:50:03 -0600

    initial

---
+++ react-complete-guide/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import "./NexExpense.css";
+import ExpenseForm from "./ExpenseForm";
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
